a230f35afa5f2729cbe305c69391ba3b650e2d7e
Ryan <rsg20@duke.edu>
2017-12-10 16:23:12 -0500

    ChangedTCP functionality and added params

--- src/BME464-photon.ino
+++ src/BME464-photon.ino
@@ -1,5 +1,6 @@
 #include "Particle.h"
 #include "string.h"
+#include "math.h"
 TCPClient client;
 int lastPublish = 0;
 String readString = "";
@@ -51,23 +52,25 @@ void loop() {
 unsigned long StartMillis=millis();
 String data = String(15);
 Particle.publish("heartparams", data, PRIVATE);
-
+client.connect("67.159.88.56",5000);
   bool PICConnect = false;
-  while(!PICConnect) {
-    if(millis()-StartMillis > 2000) {
-      break;
-    }
-      PICConnect = makeConnection();
+   while(!PICConnect) {
+     if(millis()-StartMillis > 10000) {
+       break;
+     }
+       PICConnect = makeConnection();
 
-      if(PICConnect){
-        while(numPackets<MaxNumPackets){
-          Serial1.println("radio rx 0");
-          Checker();
-          CheckerTCPPrint();
-        }
-        numPackets=0;
-      }
-  }
+       if(PICConnect){
+         while(numPackets<MaxNumPackets){
+           Serial1.println("radio rx 0");
+           Checker();
+           CheckerTCPPrint();
+         }
+         numPackets=0;
+         client.print("stop");
+       }
+   }
+client.stop();
 
 //
 // Serial1.println("radio rx 0");
@@ -131,6 +134,16 @@ void ConcatDataStrings(String s) {
         }
     }
 
+void SendDataStrings(String s){
+  String DataString=s.remove(0,8);
+  DataString=DataString.trim();
+  if(client.connected()){
+  Serial1.println("we are printing" +FullData);
+  Checker();
+  client.print(FullData);
+  }
+}
+
 void CheckerTCPPrint() {
   //Checks for the radio receive response -publishes packet once fully read
     String s = Serial1.readStringUntil('\n');
@@ -140,7 +153,8 @@ void CheckerTCPPrint() {
 bool CheckRadioRx(String s){ //Returns True when the radio received something, false when error or garbage
     String Status = s.substring(0,9);
     if(strcmp(Status, "radio_rx ")==0){
-        ConcatDataStrings(s);
+        // ConcatDataStrings(s);
+        SendDataStrings(s);
         return true;
     }
     else if(strcmp(Status, "radio_err")==0){
@@ -153,9 +167,9 @@ bool CheckRadioRx(String s){ //Returns True when the radio received something, f
 
 void gotData(const char *event, const char *data){
     str = String(data);
-    // Serial1.print("data: ");
-    // Serial1.print("\n");
-
+    Serial1.print("data: ");
+    Serial1.println(data);
+    Checker();
 
     int ch = str.indexOf("pace_channel");
     int ptd = str.indexOf("pace_time_duration");
@@ -163,13 +177,43 @@ void gotData(const char *event, const char *data){
     int pa = str.indexOf("pulse_amplitude");
     int rc = str.indexOf("record_channel");
     int rd = str.indexOf("record_duration");
+    int startrec = str.indexOf("start_record");
+    int startpace = str.indexOf("start_pace");
+    int stoppace = str.indexOf("stop_pace");
+    int paramID = str.indexOf("params_id");
 
-    // int chEnd = str.lastIndexOf('''', ptd);
-    // int ptdEnd = str.lastIndexOf(',', pwd);
-    // int pwdEnd = str.lastIndexOf(',', pa);
-    // int paEnd = str.lastIndexOf(',', rc);
-    // int rcEnd = str.lastIndexOf('''', rd);
-    // int rdEnd = str.lastIndexOf('''');
+
+    int ptdEnd = str.lastIndexOf(',', pwd);
+    int pwdEnd = str.lastIndexOf(',', pa);
+    int paEnd = str.lastIndexOf(',', rc);
+    int rdEnd = str.lastIndexOf(',',startrec);
+    int StartRecEnd = str.lastIndexOf(',',startpace);
+    int StartPaceEnd = str.lastIndexOf(',',stoppace);
+    int StopPaceEnd = str.lastIndexOf(',',paramID);
+
+    str2 = "A";//pace_channel
+    str2 +=  str.substring(ch+15,ch+16);
+    str2 += "B";//pace_time_duration
+    str2 += str.substring(ptd+20,ptdEnd);
+    str2 += "C";//pulse_width_duration
+    str2 += str.substring(pwd+22,pwdEnd);
+    str2 += "D";//pulse_amplitude
+    str2 += str.substring(pa+17,paEnd);
+    str2 += "E";//record_channel
+    str2 += str.substring(rc+17,rc+18);
+    str2 += "F";//record_duration
+    str2 += str.substring(rd+17, rdEnd);
+    str2 += "A";//start_record
+    str2 +=  str.substring(startrec+14,StartRecEnd);
+    str2 += "B";//start_pace
+    str2 += str.substring(startpace+12,StartPaceEnd);
+    str2 += "C";//stop_pace
+    str2 += str.substring(stoppace+11,StopPaceEnd);
+
+
+
+    // Serial1.print("string: ");
+    // Serial1.println(str2);
 
 
     // int m = str.indexOf("time_duration");
@@ -184,34 +228,24 @@ void gotData(const char *event, const char *data){
     //  Serial1.print("\n");
 
     //FORMING STR to send
-    str2 = "A";
-    str2+=  str.substring(ch+15,ch+16);
-    str2 += "B";
-    str2 += str.substring(ptd+20,ptd+21);
-    str2 += "C";
-    str2 += str.substring(pwd+22,pwd+23);
-    str2 += "D";
-    str2 += str.substring(pa+17,pa+19);
-    str2 += "E";
-    str2 += str.substring(rc+17,rc+18);
-    str2 += "F";
-    str2 += str.substring(rd+17,rd+19);
-    String MaxPacketStr=str.substring(rd+17,rd+19);
-
-    MaxNumPackets=MaxPacketStr.toInt();
-    //
     // str2 = "A";
-    // str2+=  str.substring(ch+15,chEnd);
-    // str2 += "B";
-    // str2 += str.substring(ptd+20,ptdEnd);
+    // str2+=  str.substring(ch+15,ch+16);
+    // str2 += "B"; int stoppace = str.indexOf("stop_pace");
+    // str2 += str.substring(ptd+20,ptd+21);
     // str2 += "C";
-    // str2 += str.substring(pwd+22,pwdEnd);
+    // str2 += str.substring(pwd+22,pwd+23);
     // str2 += "D";
-    // str2 += str.substring(pa+17,paEnd);
+    // str2 += str.substring(pa+17,pa+19);
     // str2 += "E";
-    // str2 += str.substring(rc+17,rcEnd);
+    // str2 += str.substring(rc+17,rc+18);
     // str2 += "F";
-    // str2 += str.substring(rd+17,rdEnd);
+    // str2 += str.substring(rd+17,rd+19);
+
+    String RecDurStr=str.substring(rd+17, rdEnd);
+    float RecDurFloat=RecDurStr.toFloat();
+    int MaxNumPackets=ceil(RecDurFloat/32000.);
+
+    //
     // str2.trim();
     // Serial1.print("str ");
     // Serial1.print(str2);
